d04bab88077bea81b0ea2cf51d8976bec4da21ca
Hexvg0n <118133299+Hexvg0n@users.noreply.github.com>
2024-11-07 22:27:43 +0100

    Update BestSellerSection.js

--- components/BestSellerSection.js
+++ components/BestSellerSection.js
@@ -1,4 +1,6 @@
 
+import { useRouter } from 'next/router'; // Importowanie useRouter
+
 
 export default function BestSellersSection() {
     const bestsellers = [
@@ -11,6 +13,11 @@ export default function BestSellersSection() {
         { id: 7, title: "Rick Owens Rammones",linkTo: "https://www.allchinabuy.com/en/page/buy/?url=https%3A%2F%2Fitem.taobao.com%2Fitem.htm%3Fid%3D741087802238?partnercode=wf5ZpA", image: "/images/107.png" },
         { id: 8, title: "TNF Jacket", linkTo: "https://www.allchinabuy.com/en/page/buy/?url=https%3A%2F%2Fweidian.com%2Fitem.html%3FitemID%3D6503405541%26spider_token%3D5559?partnercode=wf5ZpA", image: "/images/108.png" },
     ];
+    const router = useRouter(); // Inicjalizacja routera
+
+    const handleQCClick = (link) => {
+      router.push(`/qc?url=${encodeURIComponent(link)}`);
+    };
 
     return (
         <div className="p-8 mt-[3vh] min-h-[80vh] border-t-2 border-gray-500">
@@ -29,8 +36,8 @@ export default function BestSellersSection() {
       <div className="flex flex-col flex-grow">
         <h3 className="text-xl font-semibold text-gray-300 mb-2">{item.title}</h3>
         <div className="mt-auto flex flex-col space-y-2">
-          <button className="bg-gray-700 text-gray-300 px-4 py-2 rounded-lg hover:bg-gray-600 transition-all font-bold">
-            <a href="/qc">QC</a>
+          <button  onClick={() => handleQCClick(item.linkTo)} className="bg-gray-700 text-gray-300 px-4 py-2 rounded-lg hover:bg-gray-600 transition-all font-bold">
+            QC
           </button>
           <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-200 transition-all font-bold">
             <a href={item.linkTo}>Kup na AllChinaBuy</a>
