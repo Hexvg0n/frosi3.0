ef34fcd9facdff3b366d01d3362316d12a7509db
Hexvg0n <118133299+Hexvg0n@users.noreply.github.com>
2024-11-09 17:49:28 +0100

    QWE

--- pages/layout.js
+++ pages/layout.js
@@ -1,7 +1,26 @@
 import { Inter } from "next/font/google";
 //import "../public/css/globals.css"; 
+import Head from "next/head";
+import {metadata} from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "FrosiReps",
+  description: "Strona internetowa Frosi3.0",
+  icons: {
+    icon:[
+      '/favicon.ico'
+    ],
+    apple:[
+      '/apple-touch-icon.png'
+    ],
+    shortcuts:[
+      '/apple-touch-icon.png'
+    ]
+  },
+  manifest: '/site.webmanifest'
+};
 
 export default function RootLayout({ children }) {
   return (
